4db0aa3dc6aaf968211856785724af5cfc0cc462
Fauzan Lubis <fauzanfauh45@gmail.com>
2022-04-13 11:53:47 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Akademik Absen Automation
+
+This golang script will run a browser instance, login to your akademik account, and do all of the absen.
+
+## Usage
+
+To use this script, follow the instruction below
+
+1. Install Go
+
+   The Go languange used in this project are `go1.18`. Install Go from the official website and follow the [instructions](https://go.dev/doc/install).
+
+2. Run the script
+
+   To run the script there's a few option that you need to configure first. The script used environment variable to get the username and password of the user.
+
+   Set the `USERNAME` variable with your akademik username, and `PASSWORD` with the password of your akademik.
+
+   Then to run it, use `go run main.go`, example of how to do it in Linux,
+
+   ```bash
+   USERNAME=191524*** PASSWORD=bukanpassword go run main.go
+   ```
+
+   There's also an optional environment variable `rod` to debug the browser itself. Set it to `show` to make the browser visible.
+
+## Contributing
+
+Contribution are absolutely welcomed! Go add extra feature, add CRON functionaliy, whatever.
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-rod/rod"
+)
+
+func main() {
+	browser := rod.New().MustConnect()
+
+	defer browser.MustClose()
+
+	login_page := browser.MustPage("https://akademik.polban.ac.id")
+
+	log.Println("Starting to do login on akademik")
+
+	login_page.MustWaitLoad()
+	login_page.MustElement(`#fm > div:nth-child(1) > input`).MustInput(os.Getenv("USERNAME"))
+	login_page.MustElement(`#fm > div:nth-child(2) > input`).MustInput(os.Getenv("PASSWORD"))
+
+	login_page.MustElement(`#fm > div.row > div.col-xs-4 > button`).MustClick()
+
+	log.Println("Waiting for akademik server to response fully")
+	wait, handle := login_page.MustHandleDialog()
+	wait()
+	handle(true, "")
+
+	login_page.MustWaitLoad().MustClose()
+
+	log.Println("Login complete, closed login page, opening absen page")
+	absen_page := browser.MustPage("https://akademik.polban.ac.id/ajar/absen")
+
+	absen_page.MustWaitIdle()
+
+	log.Println("Evaluating Absen Script on the browser")
+
+	absen_page.MustAddScriptTag("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js")
+	absen_page.MustAddScriptTag("")
+	absen_page.MustEval(`
+		() => $.each($("#jadwal > tbody > tr:nth-child(n)"),(e,a)=>{const t=$(a),d=t.find("td:eq(1)").text().split("-")[0],n=t.find("td:eq(2)").text(),s=t.find("td:eq(4)").text(),l=t.find("td:eq(5)").text(),o=t.find("td:eq(6)").text(),i=document.getElementById("kls").value;$.ajax({url:"https://akademik.polban.ac.id/ajar/absen/absensi_awal",type:"POST",data:{ja:l,jb:o,mk:n,dsn:d,tp:s,kls:i},success:e=>console.log("Kelas "+n+" dosen "+d+" sudah di absenkan"),error:(e,a,t)=>{console.error("Kelas "+n+" dosen "+d+" gagal di absenkan")}})});
+	`)
+	time.Sleep(time.Minute)
+
+	log.Println("Waiting for network to go to idle")
+	absen_page.MustWaitRequestIdle()
+	log.Println("Absen should be done, making sure with a picture")
+
+	absen_page.MustScreenshot(fmt.Sprint("AbsenResult-", time.Now().Format(time.RFC3339), ".png"))
+	absen_page.MustClose()
+
+	log.Println("Absen done now, closed absen page, and now closing browser")
+}
